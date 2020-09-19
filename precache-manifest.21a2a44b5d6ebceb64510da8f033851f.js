2ae7be664e11bca448d003b0182cbf03ab27bfd2
albisk96 <albertas.kruzintaitis24@gmail.com>
2020-09-19 18:39:32 +0300

    Updates

---
+++ precache-manifest.21a2a44b5d6ebceb64510da8f033851f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02a08bc25f1ec757cb6e6c77385b9013",
+    "url": "/FlowForma/index.html"
+  },
+  {
+    "revision": "20d16515e8d8e3423b99",
+    "url": "/FlowForma/static/js/2.07b962e4.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/FlowForma/static/js/2.07b962e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a63756b37ff207d68099",
+    "url": "/FlowForma/static/js/main.ae072c50.chunk.js"
+  },
+  {
+    "revision": "e26b76136ab27dca21c5",
+    "url": "/FlowForma/static/js/runtime-main.682de922.js"
+  }
+]);
