d1c583a81e8ec87a4f63b528a78b257603545bbd
albisk96 <albertas.kruzintaitis24@gmail.com>
2020-09-21 10:09:56 +0300

    final push

--- src/components/button/switch-button.component.jsx
+++ src/components/button/switch-button.component.jsx
@@ -5,8 +5,8 @@ import { Button } from 'react-bootstrap';
 const SwitchButton = () => {
     const { matrix, toggleTheme } = useContext(UserContext);
     const render = ( matrix ?
-        <Button variant="outline-success" onClick={toggleTheme}>Normal</Button> :
-        <Button variant="outline-secondary" onClick={toggleTheme}>Matrix</Button> )
+        <Button variant="outline-success" onClick={toggleTheme}>Back to Normal</Button> :
+        <Button variant="outline-secondary" onClick={toggleTheme}>Try Matrix!</Button> )
       
     return(
         <div>{render}</div>
--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -20,7 +20,7 @@ const UserContextProvider = props => {
     )
 
     const api = axios.create({
-      baseURL: `https://78.63.13.74:3006/FlowFormaAPI`
+      baseURL: `http://78.63.13.74:3006/FlowFormaAPI`
     })
 
     const GetName = () => {
